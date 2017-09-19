2430d9ba3bc35261b3fef86073f081669aa107ab
Administrator <Administrator@DQNWR0CS2NRV87W>
2017-09-19 11:46:27 +0800

    为条件搜索添加排序功能，以及正序倒序

--- IssueFeedbackProject/WebContent/lib/js/index.js
+++ IssueFeedbackProject/WebContent/lib/js/index.js
@@ -1,62 +1,73 @@
-$(document).ready(function(){
-	$("#inlineSelectDepts").change(function(event){
+$(document).ready(function() {
+	$("#inlineSelectDepts").change(function(event) {
 		getUserByDeptId(event.target.value);
 	});
-	
-	$("#btnClickShow").on("click",function(event) {
+
+	$(".clickableRow").click(function(event) {
+		var issueId = $(this).attr("id");
+		if ($("#chb_" + issueId).prop("checked")) {
+			$("#chb_" + issueId).prop("checked", false).change();
+		} else {
+			$("#chb_" + issueId).prop("checked", true).change();
+		}
+
+	});
+	$("#btnClickShow").on("click", function(event) {
 		var arr = $(".selected");
-		var idArr = [] ;
-		$.each(arr,function(index,element){
+		var idArr = [];
+		$.each(arr, function(index, element) {
 			idArr.push($(element).val());
 		});
 		var strParam = "";
-		$.each(idArr,function(index,element){
-			if(index != idArr.length-1){
-				strParam = strParam.concat(element+",");
-			}else{
+		$.each(idArr, function(index, element) {
+			if (index != idArr.length - 1) {
+				strParam = strParam.concat(element + ",");
+			} else {
 				strParam = strParam.concat(element);
 			}
 		});
-			
-		console.log(strParam);
+
 		$.ajax({
-			url:"/IssueFeedbackProject/Export",
-			data:{
-				arr:strParam
+			url : "/IssueFeedbackProject/Export",
+			data : {
+				arr : strParam
 			},
-			contentType: "application/json;charset=utf-8",
-			success:function(data,event){
-				console.log(idArr);
+			contentType : "application/json;charset=utf-8",
+			success : function(data, event) {
+				if (data.errorCode == -1) {
+					window.location = "/IssueFeedbackProject/Download";
+				}
 			}
 		})
 	});
-	$(".selectedCheckbox").change(function(event){
+	$(".selectedCheckbox").change(function(event) {
 		var element = event.target;
-		if(element.checked){
+		if (element.checked) {
 			$(this).addClass("selected");
-		}else{
+		} else {
 			$(this).removeClass("selected");
 		}
-		
 	});
 });
 
-
-function getUserByDeptId(id){
-	$.ajax({
-		url:"/IssueFeedbackProject/DeptUsers",
-		data:{
-			dept_id:id
-		},
-		contentType: "application/json;charset=utf-8",
-		success:function(data,event){
-			$("#selectUsers").empty();
-			var defaultElement = $("<option>").text("全部用户").attr("value","-1");
-			$("#selectUsers").append(defaultElement);
-			$.each(data,function(index,user){
-				var element = $("<option>").text(user.realName).attr("value",user.id);
-				$("#selectUsers").append(element);
-			});
-		}
-	})
+function getUserByDeptId(id) {
+	$
+			.ajax({
+				url : "/IssueFeedbackProject/DeptUsers",
+				data : {
+					dept_id : id
+				},
+				contentType : "application/json;charset=utf-8",
+				success : function(data, event) {
+					$("#selectUsers").empty();
+					var defaultElement = $("<option>").text("全部用户").attr(
+							"value", "-1");
+					$("#selectUsers").append(defaultElement);
+					$.each(data, function(index, user) {
+						var element = $("<option>").text(user.realName).attr(
+								"value", user.id);
+						$("#selectUsers").append(element);
+					});
+				}
+			})
 }
