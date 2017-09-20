9ae1eda3a3e8af235ec204edd0d9b6ca39d60dd4
Administrator <Administrator@DQNWR0CS2NRV87W>
2017-09-20 09:35:43 +0800

    添加为问题标记为疑难问题的功能，并只有研发二部的也可以修改问题状态

---
+++ IssueFeedbackProject/src/com/xwj/dao/CommentDao.java
@@ -0,0 +1,17 @@
+package com.xwj.dao;
+
+public class CommentDao {
+
+//	private DbUtils dbUtils;
+	public CommentDao() {
+//		dbUtils = DbUtils.newInstance();
+	}
+	
+	public void insertImage() {
+		
+	}
+	
+	public void addImageToComment() {
+		
+	}
+}
